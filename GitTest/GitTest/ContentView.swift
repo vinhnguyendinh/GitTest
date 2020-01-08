fe331b034272f48b5fbbb9874638474803c22d03
Vinh Nguyen <nguyen-dinh-vinh@goodcycle.net>
2020-01-08 17:17:32 +0900

    update button

--- GitTest/GitTest/ContentView.swift
+++ GitTest/GitTest/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+
+            Button(action: {
+                print("123")
+            }) {
+                Text("Click here")
+            }
+        }
     }
 }
 
