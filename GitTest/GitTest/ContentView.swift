2129b5690d67468aee271456a586ee2196aca30a
Vinh Nguyen <nguyen-dinh-vinh@goodcycle.net>
2020-01-08 17:15:14 +0900

    first commit

---
+++ GitTest/GitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by Vinh Nguyen Dinh on 2020/01/08.
+//  Copyright © 2020 Vinh Nguyen Dinh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
